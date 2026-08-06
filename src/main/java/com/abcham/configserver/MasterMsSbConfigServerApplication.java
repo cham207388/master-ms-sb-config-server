a6cbbaa1368f4b33059d50bb3f97721717e3aee5
cham207388 <cham207388@gmail.com>
2026-08-05 19:18:58 -0500

    first commit

---
+++ src/main/java/com/abcham/configserver/MasterMsSbConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.abcham.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MasterMsSbConfigServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MasterMsSbConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abcham/configserver/MasterMsSbConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abcham.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterMsSbConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
